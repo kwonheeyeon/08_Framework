d613e584756bafc8f0eacaafdb70f08b6a8ec3a3
kwonheeyeon <hingni0911@gmail.com>
2024-09-13 15:17:49 +0900

    24.09.13 boardProject - myPage

---
+++ boardProject/src/main/java/edu/kh/project/myPage/mapper/MyPageMapper.java
@@ -0,0 +1,27 @@
+package edu.kh.project.myPage.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import edu.kh.project.member.dto.Member;
+
+@Mapper // 상속받은 클래스 구현 + Bean 등록
+public interface MyPageMapper {
+
+	/** 회원 정보 수정
+	 * 
+	 * @param inputMember
+	 * @return result
+	 */
+	int updateInfo(Member inputMember);
+	
+	/** (비동기) 닉네임 중복 검사
+	 * 
+	 * @param input
+	 * 
+	 * @return 0 : 중복 X / 1 : 중복 O
+	 */
+	int checkNickname(String input);
+	
+	
+
+}
